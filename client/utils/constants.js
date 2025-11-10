dec2382d52afcfc68d35a2e862a7192147526137
ievlev_alex <ievlev@outlook.com>
2025-11-10 14:36:01 +0200

    Deploy public folder to gh-pages

---
+++ client/utils/constants.js
@@ -0,0 +1,13 @@
+export const STANDARD_KEYS = new Set([
+  "publisherId",
+  "aliasName",
+  "isActive",
+  "pages",
+  "publisherDashboard",
+  "monitorDashboard",
+  "qaStatusDashboard",
+  "customCss",
+  "tags",
+  "notes",
+]);
+export const BASE_URL = "/data";
